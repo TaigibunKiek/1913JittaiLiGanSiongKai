ae391fbf4c3a4baa87621d84a44aba16bdbc87d1
Ngô͘ Hê-bí <shiami@users.noreply.github.com>
2025-04-05 07:06:27 +0800

    Ap-té chu-liāu.

--- assets/javascripts/viewer.js
+++ assets/javascripts/viewer.js
@@ -1,11 +1,11 @@
 (function () {
   // TODO edit
-  const baseUrl = '/scannedImgViewer';
+  const baseUrl = '/1913JittaiLiGanSiongKai';
   const dataFolderPath = '/chuliau';
-  const dataNamePrefix = '/TJTST1_';
+  const dataNamePrefix = '/1913JTLGSK_Page_';
   const dataNameNumberCount = 3;
   const dataNameSuffix = '.jpg';
-  const lastPageNumber = 12;
+  const lastPageNumber = 298;
 
   let imgSources = [];
   const imgSourcesMin = 1;
